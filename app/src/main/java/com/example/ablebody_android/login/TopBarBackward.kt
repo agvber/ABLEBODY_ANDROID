46bc2a9cc465f2bc58e8e5ae8493e61785574d64
nhkim <nhkim1529@gmail.com>
2023-07-12 00:29:13 +0900

    Rename: TopBarBackward 함수명 오타 수정

--- app/src/main/java/com/example/ablebody_android/login/TopBarBackward.kt
+++ app/src/main/java/com/example/ablebody_android/login/TopBarBackward.kt
@@ -6,22 +6,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ablebody_android.R
-import com.example.ablebody_android.ui.theme.AbleLight
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
