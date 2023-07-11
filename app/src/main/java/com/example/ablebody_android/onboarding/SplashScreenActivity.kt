a89f2b0d7ff3b828b45a17bec5b0e10866d92ad7
nhkim <nhkim1529@gmail.com>
2023-07-12 00:39:16 +0900

    Rename: 파일명 변경
    SplashScreen -> SplashScreenActivity

--- app/src/main/java/com/example/ablebody_android/onboarding/SplashScreenActivity.kt
+++ app/src/main/java/com/example/ablebody_android/onboarding/SplashScreenActivity.kt
@@ -3,14 +3,8 @@ package com.example.ablebody_android.onboarding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,7 +12,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.ablebody_android.onboarding.utils.compose.LogoImage
 import kotlinx.coroutines.delay
 
-class SplashScreen : ComponentActivity() {
+class SplashScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
