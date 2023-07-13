f711c81bef375dab2306f3f249b765b4bda72970
minjun <agvber12@gmail.com>
2023-07-13 19:04:42 +0900

    Style : OnboardingApiUnitTest 파일 위치 변경

--- app/src/androidTest/java/com/example/ablebody_android/OnboardingApiUnitTest.kt
+++ app/src/androidTest/java/com/example/ablebody_android/OnboardingApiUnitTest.kt
@@ -1,14 +1,23 @@
 package com.example.ablebody_android
 
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert
 import org.junit.Test
+import org.junit.runner.RunWith
 
+
+@RunWith(AndroidJUnit4::class)
 class OnboardingApiUnitTest {
 
-    private val networkRepository = NetworkRepository()
+    private val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    private val tokenSharedPreferencesRepository = TokenSharedPreferencesRepository(applicationContext = appContext)
+    private val networkRepository = NetworkRepository(tokenSharedPreferencesRepository)
 
     private val authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhdXRoLXRva2VuIiwidWlkIjoiODc1NTM4MCIsImV4cCI6MTY4OTE1MTM0N30.o96O1ijkZreWOMHllSHxcxR3xQ6yvlvA7j1XneXORoq_7aquOOwxMTa-ShFeOUdp0PaqiYm2ZIdMTSqXarqJjA"
-    private val refreshToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZWZyZXNoLXRva2VuIiwidWlkIjoiODc1NTM4MCIsImV4cCI6MTY5MDM1OTE0N30.bxdYxJpNSPl1tk-n-MX27wRRGHO7L6JKwGZsxJHLKrTl2B5u64r8TD8M2tiOKj-PXLVZEBBKMBIPTA1bpdA_nA"
+    private val refreshToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZWZyZXNoLXRva2VuIiwidWlkIjoiODc1NTM4MCIsImV4cCI6MTY5MDQ0OTg2OH0.j9j-yOX5JNt4ENCA-6A4KuLpNoBK6CiBwLbJwW1nuPVZEUFJYp1DxVs03p3eUg89qG8CJNZ-BgieN4oaWG6MXA"
 
     @Test
     fun sendSMS() {
@@ -56,7 +65,7 @@ class OnboardingApiUnitTest {
 
     @Test
     fun refreshToken() {
-        val response = networkRepository.refreshToken(refreshToken = refreshToken)
+        val response = networkRepository.getRefreshToken(refreshToken = refreshToken)
         println("response: $response, body: ${response.body()}")
         Assert.assertEquals(response.code(), 200)
     }
