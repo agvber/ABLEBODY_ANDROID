c8a6ad412a03523f31dfda8b3c3cb52c12818c9b
nhkim <nhkim1529@gmail.com>
2023-07-22 01:30:21 +0900

    Style: PhonenumberRule 수정
    Nothing의 false -> true

--- app/src/main/java/com/example/ablebody_android/onboarding/data/PhonenumberRule.kt
+++ app/src/main/java/com/example/ablebody_android/onboarding/data/PhonenumberRule.kt
@@ -3,5 +3,5 @@ package com.example.ablebody_android.onboarding.data
 enum class PhonenumberRule(val positive: Boolean, val description: String) {
     Correct(true, "사용 가능한 닉네임이에요."),
     Unstyled(false, "휴대폰 번호 양식에 맞지 않아요."),
-    Nothing(false, "분 초 남음")
+    Nothing(true, "분 초 남음")
 }
