944b479c8e6b3fd19dc7ce11e9b6fee800ada58a
nhkim <nhkim1529@gmail.com>
2023-07-23 21:37:59 +0900

    Feat: InputPhoneNumberScreen 양식에 맞는 번호 입력시에만 버튼 활성화
    InputPhoneNumberwithoutRuleLayout : 규칙 없는 휴대폰 번호 입력창
    InputPhoneNumberwithRuleLayout : 규칙 있는 휴대폰 번호 입력창
    PhoneNumberFormRule : 휴대폰 번호 입력 규칙
    PhonenumberFormJudgment : 버튼 활성화 판단 함수
    PhonenumberJoinExplanation : 휴대폰 번호 가입 설명

--- app/src/main/java/com/example/ablebody_android/onboarding/InputPhoneNumberScreen.kt
+++ app/src/main/java/com/example/ablebody_android/onboarding/InputPhoneNumberScreen.kt
@@ -21,52 +21,10 @@ import com.example.ablebody_android.utils.HighlightText
 import com.example.ablebody_android.ui.theme.AbleBlue
 import com.example.ablebody_android.ui.theme.AbleDark
 import com.example.ablebody_android.utils.TextFieldUnderCorrectText
+import com.example.ablebody_android.utils.TextFieldUnderText
 import com.example.ablebody_android.utils.TextFieldUnderWrongText
-
 @Composable
-fun ShowPhoneNumberRule(
-    phonenumber: String,
-){
-    if (phonenumber=="correct") {
-        TextFieldUnderCorrectText("")
-    }
-    else if(phonenumber=="unstyled"){
-        TextFieldUnderWrongText("휴대폰 번호 양식에 맞지 않아요." )
-    }
-    else if(phonenumber=="nothing"){
-        TextFieldUnderCorrectText("분 초 남음")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ShowPhoneNumberRulePreview() {
-    ShowPhoneNumberRule("correct")
-}
-
-@Composable
-fun CheckPhoneNumberRule(
-    value: String,
-) {
-    val path = value
-    val regex = "^01[0-1, 7][0-9]{8}\$".toRegex()
-
-    if (value == "") ShowPhoneNumberRule("nothing")
-    else{
-        if (isPhoneNumberRuleMatch(path, regex)) {
-            ShowPhoneNumberRule("correct")
-        }
-        else{
-            ShowPhoneNumberRule("unstyled")
-        }
-    }
-}
-
-@Composable
-fun isPhoneNumberRuleMatch(path: String, regex: Regex): Boolean = path.matches(regex)
-
-@Composable
-fun InputPhoneNumberLayout(
+fun InputPhoneNumberwithoutRuleLayout(
     value: String,
     onValueChange: (String) -> Unit
 ) {
@@ -79,10 +37,10 @@ fun InputPhoneNumberLayout(
 
 @Preview(showBackground = true)
 @Composable
-fun InputPhoneNumberLayoutPreview() {
+fun InputPhoneNumberwithoutRuleLayoutPreview() {
     var textState by remember { mutableStateOf("") }
 
-    InputPhoneNumberLayout(
+    InputPhoneNumberwithoutRuleLayout(
         value = textState,
         onValueChange = { textState = it }
     )
@@ -94,10 +52,8 @@ fun InputPhoneNumberwithRuleLayout(
     onValueChange: (String) -> Unit
 ) {
     Column {
-        CustomTextField(
-            labelText = "휴대폰 번호", value = value, onValueChange = onValueChange,
-        )
-        CheckPhoneNumberRule(value)
+        InputPhoneNumberwithoutRuleLayout(value, onValueChange)
+        PhoneNumberFormRule(value)
     }
 
 }
@@ -111,7 +67,54 @@ fun InputPhoneNumberwithRuleLayoutPreview() {
         onValueChange = { textState = it }
     )
 }
+@Composable
+fun PhoneNumberFormRule(
+    value: String,
+) {
+    val path = value
+    val regex = "^01[0-1, 7][0-9]{8}\$".toRegex()
 
+    if (value == "") {
+        TextFieldUnderText("분 초 남음", true)
+    }
+    else{
+        if (isPhoneNumberRuleMatch(path, regex)) {
+            TextFieldUnderText("", true)
+        }
+        else{
+            TextFieldUnderText("휴대폰 번호 양식에 맞지 않아요.", false)
+        }
+    }
+}
+@Composable
+fun isPhoneNumberRuleMatch(path: String, regex: Regex): Boolean = path.matches(regex)
+
+@Composable
+fun PhonenumberFormJudgment(
+    phonenumber: String,
+) : Boolean{
+
+    val path = phonenumber
+    val regex = "^01[0-1, 7][0-9]{8}\$".toRegex()
+
+    return !(phonenumber=="" || !isPhoneNumberRuleMatch(path, regex))
+}
+
+@Composable
+fun PhonenumberJoinExplanation(){
+    HighlightText(
+        string = "애블바디는 휴대폰 번호로 가입해요.\n휴대폰 번호는 안전하게 보관되며\n어디에도 공개되지 않아요.",
+        colorStringList = listOf("휴대폰 번호"),
+        color = AbleBlue,
+        style = TextStyle(
+            fontSize = 22.sp,
+            lineHeight = 35.sp,
+            fontWeight = FontWeight(700),
+            color = AbleDark,
+            fontFamily = FontFamily(Font(R.font.noto_sans_cjkr_black))
+        )
+    )
+}
 
 @Composable
 private fun InputPhoneNumberContent(
@@ -119,18 +122,7 @@ private fun InputPhoneNumberContent(
     onValueChange: (String) -> Unit
 ) {
     Column(modifier = Modifier.padding(horizontal = 16.dp)) {
-        HighlightText(
-            string = "애블바디는 휴대폰 번호로 가입해요.\n휴대폰 번호는 안전하게 보관되며\n어디에도 공개되지 않아요.",
-            colorStringList = listOf("휴대폰 번호"),
-            color = AbleBlue,
-            style = TextStyle(
-                fontSize = 22.sp,
-                lineHeight = 35.sp,
-                fontWeight = FontWeight(700),
-                color = AbleDark,
-                fontFamily = FontFamily(Font(R.font.noto_sans_cjkr_black))
-            )
-        )
+        PhonenumberJoinExplanation()
         InputPhoneNumberwithRuleLayout(value, onValueChange)
     }
 }
@@ -139,9 +131,9 @@ private fun InputPhoneNumberContent(
 @Composable
 private fun ContentPreview() {
 
-    var state by remember{ mutableStateOf("") }
+    var phoneNumberState by remember{ mutableStateOf("") }
+    InputPhoneNumberContent(phoneNumberState) { phoneNumberState = it }
 
-    InputPhoneNumberContent(state) { state = it }
 }
 
 
@@ -149,24 +141,17 @@ private fun ContentPreview() {
 fun InputPhoneNumberScreen(
     viewModel: OnboardingViewModel = viewModel()
 ) {
-
     var phoneNumberState by remember{ mutableStateOf("") }
-
-    val isNotPhonenumberDuplicate by viewModel.isNotPhonenumberDuplicate.observeAsState()
+//    val isNotPhonenumberDuplicate by viewModel.isNotPhonenumberDuplicate.observeAsState()
 
     BottomCustomButtonLayout(
         buttonText = "인증번호 받기",
         onClick = {
             viewModel.checkDuplicatePhonenumber(phoneNumberState)
-            Log.d("LOG", "버튼 눌려짐!")
-        }
-//                Log.d("LOG", "버튼 눌려짐!")
+        },
+        enable = PhonenumberFormJudgment(phoneNumberState)
     ) {
-
-        InputPhoneNumberContent(phoneNumberState) {
-            //키패드가 눌려질 때마다
-            phoneNumberState = it
-        }
+        InputPhoneNumberContent(phoneNumberState) { phoneNumberState = it }
     }
 }
 
