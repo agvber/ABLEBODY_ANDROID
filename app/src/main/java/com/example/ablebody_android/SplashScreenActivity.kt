072a08c33554148d58c84a50e2081365e1c022a5
minjun <agvber12@gmail.com>
2023-07-28 22:24:03 +0900

    Fix: Splash 스크린 수정

--- app/src/main/java/com/example/ablebody_android/SplashScreenActivity.kt
+++ app/src/main/java/com/example/ablebody_android/SplashScreenActivity.kt
@@ -1,34 +1,45 @@
 package com.example.ablebody_android
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.splashscreen.SplashScreenViewProvider
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.ablebody_android.onboarding.OnboardingActivity
 import com.example.ablebody_android.utils.LogoImage
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SplashPartLayout(this)
-        }
+
+        val splashScreen = installSplashScreen()
+
+        splashScreen.setKeepOnScreenCondition { true }
+
+        startActivity(Intent(this@SplashScreenActivity, OnboardingActivity::class.java))
+        finish()
     }
 }
 
 @Composable
-fun SplashPartLayout(context: Context) {
+fun SplashPartLayout() {
     LogoImage()
-    LaunchedEffect(Unit){
-        delay(1000L)
-        context.startActivity(Intent(context, OnboardingActivity::class.java))
-    }
 }
 
 
