6f1bbe47ef7cea51c41d61c295826e64a7ae29fe
minjun <agvber12@gmail.com>
2023-08-15 22:23:12 +0900

    Move: mainActivity main 폴더로 이동

--- app/src/main/java/com/example/ablebody_android/main/utils/ui/AbleBodyBottomNavigationItem.kt
+++ app/src/main/java/com/example/ablebody_android/main/utils/ui/AbleBodyBottomNavigationItem.kt
@@ -1,4 +1,4 @@
-package com.example.ablebody_android.main.ui.utils
+package com.example.ablebody_android.main.utils.ui
 
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.TweenSpec
