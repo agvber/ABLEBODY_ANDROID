87c317561b98f80259aeef4541fa19848f8f6e00
minjun <agvber12@gmail.com>
2023-08-31 01:48:17 +0900

    Rename: SortingMethod에서 NAME을 ALPHABET으로 변경

--- app/src/main/java/com/example/ablebody_android/SortingMethod.kt
+++ app/src/main/java/com/example/ablebody_android/SortingMethod.kt
@@ -1,9 +1,8 @@
 package com.example.ablebody_android
 
-import com.google.gson.annotations.SerializedName
 
 enum class SortingMethod {
     POPULAR,
     RECENT,
-    @SerializedName("ALPHABET") NAME
+    ALPHABET
 }
