a7423e116c7bd778dc67faec7abf00d4ae25a988
minjun <agvber12@gmail.com>
2023-09-01 19:39:51 +0900

    Refactor: MainScreen과 MainNavigationBar 분리

--- app/src/main/java/com/example/ablebody_android/main/ui/MainScreen.kt
+++ app/src/main/java/com/example/ablebody_android/main/ui/MainScreen.kt
@@ -1,41 +1,12 @@
 package com.example.ablebody_android.main.ui
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.ablebody_android.brand.BrandViewModel
 import com.example.ablebody_android.brand.ui.BrandScreen
-import com.example.ablebody_android.main.data.NavigationItems
-import com.example.ablebody_android.main.ui.utils.AbleBodyBottomNavigationItem
-import com.example.ablebody_android.ui.theme.AbleDark
-import com.example.ablebody_android.ui.theme.AbleLight
-import com.example.ablebody_android.ui.theme.White
 import com.example.ablebody_android.utils.ItemSearchBar
 
 @Composable
@@ -61,69 +32,4 @@ fun MainScreen(viewModel: BrandViewModel = androidx.lifecycle.viewmodel.compose.
 @Composable
 fun MainScreenPreview() {
     MainScreen()
-}
-
-@Composable
-fun MainNavigationBar() {
-    BottomNavigation(
-        modifier = Modifier
-            .shadow(
-                elevation = 3.dp,
-                shape = RoundedCornerShape(topStart = 25.dp, topEnd = 25.dp),
-                ambientColor = Color.Black,
-                spotColor = Color.Black
-            )
-            .fillMaxWidth()
-            .height(78.dp)
-        ,
-        backgroundColor = White
-    ) {
-        var selectedItem by remember { mutableStateOf(NavigationItems.Brand) }
-
-        LazyRow(
-            modifier = Modifier
-                .fillMaxWidth()
-                .selectableGroup(),
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            items(items = NavigationItems.values()) { item ->
-                val animateColor by animateColorAsState(
-                    targetValue = if (selectedItem == item) AbleDark else AbleLight
-                )
-
-                AbleBodyBottomNavigationItem(
-                    selected = item == selectedItem,
-                    onClick = { selectedItem = item },
-                    icon = {
-                        Icon(
-                            painter = painterResource(item.imageEnableResourceID),
-                            contentDescription = null,
-                            tint = animateColor
-                        )
-                           },
-                    label = {
-                        Text(
-                            text = item.labelText,
-                            style = TextStyle(
-                                fontSize = 10.sp,
-                                fontWeight = FontWeight(500),
-                                color = animateColor,
-                                textAlign = TextAlign.Center,
-                            )
-                        )
-                    },
-                    alwaysShowLabel = true,
-                    modifier = Modifier
-                        .padding(top = 4.dp, bottom = 24.dp)
-                        .clip(RoundedCornerShape(15.dp))
-                )
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainNavigationBarPreview() {
-    MainNavigationBar()
 }
