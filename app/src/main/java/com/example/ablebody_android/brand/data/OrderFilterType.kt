e0c69665500cd7e8a2890bdc3d7ed83d7ee78b57
minjun <agvber12@gmail.com>
2023-09-02 17:44:41 +0900

    Remove: 사용하지 않는 파일 제거

--- app/src/main/java/com/example/ablebody_android/brand/data/OrderFilterType.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ablebody_android.brand.data
-
-import com.example.ablebody_android.R
-
-enum class OrderFilterType(val stringResourceID: Int) {
-    Popularity(R.string.brand_order_filter_type_popularity),
-    Name(R.string.brand_gender_filter_type_name)
-}
