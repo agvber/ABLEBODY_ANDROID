34c57e7c5e198cbfee3e99560e82b00ffaefb1f4
minjun <agvber12@gmail.com>
2023-10-12 16:14:01 +0900

    Feat: LikeListGraph.kt 추가

--- app/src/main/java/com/smilehunter/ablebody/presentation/like_list/LikeListGraph.kt
+++ app/src/main/java/com/smilehunter/ablebody/presentation/like_list/LikeListGraph.kt
@@ -1,6 +1,41 @@
 package com.smilehunter.ablebody.presentation.like_list
 
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.smilehunter.ablebody.presentation.like_list.ui.LikeListRoute
 
-fun NavGraphBuilder.addLikeListScreen() {
+
+const val LikeUserListRoute = "like_user_list_route"
+
+fun NavController.navigateToLikeUserListScreen(
+    contentID: Long,
+    navOptions: NavOptions? = null,
+) {
+    navigate(route = "$LikeUserListRoute/$contentID", navOptions = navOptions)
+}
+
+
+fun NavGraphBuilder.addLikeUserListScreen(
+    isBottomBarShow: (Boolean) -> Unit,
+    onBackRequest: () -> Unit,
+    profileRequest: (String) -> Unit,
+) {
+    
+    composable(
+        route = "$LikeUserListRoute/{content_id}",
+        arguments = listOf(
+            navArgument("content_id") { type = NavType.LongType }
+        )
+    ) {  backStackEntry ->
+        LikeListRoute(
+            onBackRequest = onBackRequest,
+            profileRequest = profileRequest,
+            contentID = backStackEntry.arguments?.getLong("content_id", 0)!!
+        )
+        isBottomBarShow(false)
+    }
 }
