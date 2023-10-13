6bcbeca4a5606170f50bfb6f362ef599b97f7c7f
minjun <agvber12@gmail.com>
2023-10-13 10:41:06 +0900

    Test: UserApiUnitTest 추가

---
+++ app/src/test/java/com/smilehunter/ablebody/network/UserApiUnitTest.kt
@@ -0,0 +1,21 @@
+package com.smilehunter.ablebody.network
+
+import com.smilehunter.ablebody.network.utils.TestRetrofit
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class UserApiUnitTest {
+    private val networkService: NetworkService = TestRetrofit.getInstance()
+
+    @Test
+    fun getMyUserData() {
+        val response = runBlocking { networkService.getMyUserData() }
+        println(response)
+    }
+
+    @Test
+    fun getUserData() {
+        val response = runBlocking { networkService.getUserData(uid = "5920702") }
+        println(response)
+    }
+}
