3902ea302362b7582fdd829c5a58f3de618d9f64
minjun <agvber12@gmail.com>
2023-10-18 18:21:17 +0900

    Modify: 새로운 API DTO 지원

--- app/src/main/java/com/smilehunter/ablebody/data/dto/response/AbleBodyResponse.kt
+++ app/src/main/java/com/smilehunter/ablebody/data/dto/response/AbleBodyResponse.kt
@@ -3,8 +3,26 @@ package com.smilehunter.ablebody.data.dto.response
 import com.google.gson.annotations.SerializedName
 
 data class AbleBodyResponse<out T>(
-    val code: Int,
-    val message: String,
-    val success: Boolean,
+    val errorCode: ErrorCode?,
+    val code: Int?,
+    val message: String?,
+    @SerializedName(value = "success", alternate = ["is_success"]) val success: Boolean,
     @SerializedName(value = "data", alternate = ["dataList"]) val data: T?
-)
+) {
+    enum class ErrorCode {
+        NULL_TOKEN,
+        INVALID_TOKEN,
+        EXPIRED_TOKEN,
+        MEMBER_NOT_FOUND,
+        MEMBER_NOT_ALLOWED,
+        INVALID_MEMBERINFO,
+        DUPLICATE_EMAIL,
+        DUPLICATE_NICKNAME,
+        INVALID_ITEM,
+        INVALID_ADDRESS,
+        UNABLE_COUPON,
+        EXIST_ADDRESS,
+        NON_EXIST_ADDRESS,
+        INVALID_OPTION
+    }
+}
