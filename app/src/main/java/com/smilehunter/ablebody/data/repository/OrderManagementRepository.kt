0f13e34d8c069b87e665c0d565fa63f3ad9fd769
MinJun <agvber12@gmail.com>
2023-10-28 16:10:24 +0900

    Feat: 사용자가 아이템 주문을 요청하는 함수 추가

--- app/src/main/java/com/smilehunter/ablebody/data/repository/OrderManagementRepository.kt
+++ app/src/main/java/com/smilehunter/ablebody/data/repository/OrderManagementRepository.kt
@@ -1,5 +1,6 @@
 package com.smilehunter.ablebody.data.repository
 
+import com.smilehunter.ablebody.data.dto.response.AddOrderListResponse
 import com.smilehunter.ablebody.data.dto.response.GetDeliveryInfoResponse
 import com.smilehunter.ablebody.data.dto.response.GetOrderListResponse
 
@@ -10,4 +11,21 @@ interface OrderManagementRepository {
     suspend fun cancelOrderItem(id: String)
 
     suspend fun getDeliveryTrackingNumber(id: String): GetDeliveryInfoResponse
+
+    suspend fun orderItem(
+        itemID: Int,
+        addressID: Int,
+        couponBagsID: Int?,
+        refundBankName: String,
+        refundAccount: String,
+        refundAccountHolder: String,
+        paymentMethod: String,
+        price: Int,
+        itemDiscount: Int,
+        couponDiscount: Int,
+        pointDiscount: Int,
+        deliveryPrice: Int,
+        amountOfPayment: Int,
+        itemOptionIdList: List<Long>?
+    ): AddOrderListResponse
 }
--- app/src/main/java/com/smilehunter/ablebody/data/repository/OrderManagementRepositoryImpl.kt
+++ app/src/main/java/com/smilehunter/ablebody/data/repository/OrderManagementRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.smilehunter.ablebody.data.repository
 
+import com.smilehunter.ablebody.data.dto.response.AddOrderListResponse
 import com.smilehunter.ablebody.data.dto.response.GetDeliveryInfoResponse
 import com.smilehunter.ablebody.data.dto.response.GetOrderListResponse
 import com.smilehunter.ablebody.network.NetworkService
@@ -20,5 +21,38 @@ class OrderManagementRepositoryImpl @Inject constructor(
         return networkService.getDeliveryInfo(id)
     }
 
+    override suspend fun orderItem(
+        itemID: Int,
+        addressID: Int,
+        couponBagsID: Int?,
+        refundBankName: String,
+        refundAccount: String,
+        refundAccountHolder: String,
+        paymentMethod: String,
+        price: Int,
+        itemDiscount: Int,
+        couponDiscount: Int,
+        pointDiscount: Int,
+        deliveryPrice: Int,
+        amountOfPayment: Int,
+        itemOptionIdList: List<Long>?
+    ): AddOrderListResponse {
+        return networkService.addOrderList(
+            itemID = itemID,
+            addressID = addressID,
+            couponBagsID = couponBagsID,
+            refundBankName = refundBankName,
+            refundAccount = refundAccount,
+            refundAccountHolder = refundAccountHolder,
+            paymentMethod = paymentMethod,
+            price = price,
+            itemDiscount = itemDiscount,
+            couponDiscount = couponDiscount,
+            pointDiscount = pointDiscount,
+            deliveryPrice = deliveryPrice,
+            amountOfPayment = amountOfPayment,
+            itemOptionIdList = itemOptionIdList
+        )
+    }
 
 }
