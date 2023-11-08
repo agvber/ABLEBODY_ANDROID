fbbc9b302417ec4b955723d050a5b90ae94a03e5
nhkim <nhkim1529@gmail.com>
2023-11-09 00:09:51 +0900

    Feat: MyProfileViewModel에서 getUserInfoUseCase 사용

--- app/src/main/java/com/smilehunter/ablebody/presentation/my/MyProfileViewModel.kt
+++ app/src/main/java/com/smilehunter/ablebody/presentation/my/MyProfileViewModel.kt
@@ -2,16 +2,19 @@ package com.smilehunter.ablebody.presentation.my
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.smilehunter.ablebody.domain.GetUserInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MyProfileViewModel @Inject constructor(
+    private val getUserInfoUseCase: GetUserInfoUseCase
 //    private val myRepository: MyRepository
 ): ViewModel() {
     fun getData(id: Long){
         viewModelScope.launch {
+            getUserInfoUseCase()
         }
     }
 }
