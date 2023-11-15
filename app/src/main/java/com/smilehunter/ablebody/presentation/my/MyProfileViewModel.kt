73fdacce5cab06f91720c87ac4963fb3800c7ced
nhkim <nhkim1529@gmail.com>
2023-11-15 20:50:05 +0900

    Feat: 크리에이터의 코디 사진들을 불러올 GetUserBoardPagerUseCase 사용

--- app/src/main/java/com/smilehunter/ablebody/presentation/my/MyProfileViewModel.kt
+++ app/src/main/java/com/smilehunter/ablebody/presentation/my/MyProfileViewModel.kt
@@ -1,23 +1,32 @@
 package com.smilehunter.ablebody.presentation.my
 
+import android.util.Log
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.smilehunter.ablebody.data.repository.UserRepository
 import com.smilehunter.ablebody.domain.GetCouponListUseCase
 import com.smilehunter.ablebody.domain.GetOrderItemListUseCase
+import com.smilehunter.ablebody.domain.GetUserBoardPagerUseCase
 import com.smilehunter.ablebody.domain.GetUserInfoUseCase
 import com.smilehunter.ablebody.domain.ItemData
 import com.smilehunter.ablebody.model.CouponData
 import com.smilehunter.ablebody.model.OrderItemData
+import com.smilehunter.ablebody.model.UserBoardData
 import com.smilehunter.ablebody.model.UserInfoData
 import com.smilehunter.ablebody.network.di.AbleBodyDispatcher
 import com.smilehunter.ablebody.network.di.Dispatcher
 import com.smilehunter.ablebody.presentation.item_detail.ui.ItemDetailViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.WhileSubscribed
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -27,14 +36,11 @@ class MyProfileViewModel @Inject constructor(
     private val getUserInfoUseCase: GetUserInfoUseCase,
     private val getCouponListUseCase: GetCouponListUseCase,
     private val getOrderItemListUseCase: GetOrderItemListUseCase,
+    private val getUserBoardPagerUseCase: GetUserBoardPagerUseCase,
     @Dispatcher(AbleBodyDispatcher.IO) private val ioDispatcher: CoroutineDispatcher
-//    private val myRepository: MyRepository
+
 ): ViewModel() {
-//    fun getData(){
-//        viewModelScope.launch {
-//            getUserInfoUseCase()
-//        }
-//    }
+
     private val _userInfoLiveData = MutableLiveData<UserInfoData>()
     val userLiveData: LiveData<UserInfoData> = _userInfoLiveData
 
@@ -45,12 +51,23 @@ class MyProfileViewModel @Inject constructor(
     private val _orderItemListLiveData = MutableLiveData<List<OrderItemData>>()
     val orderItemListLiveData: LiveData<List<OrderItemData>> = _orderItemListLiveData
 
+    val userBoard: StateFlow<PagingData<UserBoardData.Content>>
+            = getUserBoardPagerUseCase()
+        .cachedIn(viewModelScope)
+        .stateIn(
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            PagingData.empty()
+        )
+
 
     fun getData() {
         viewModelScope.launch {
             try {
                 val userInfo = getUserInfoUseCase.invoke()
                 _userInfoLiveData.postValue(userInfo)
+
+                Log.d("userInfo", userInfo.toString())
 
                 val couponList = getCouponListUseCase.invoke()
                 _couponListLiveData.postValue(couponList)
