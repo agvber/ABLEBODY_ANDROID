c17589337bab2b638331ed99767423cd2dafa69b
nhkim <nhkim1529@gmail.com>
2023-11-15 20:50:28 +0900

    Feat: 스레드를 ioDispatcher로 고정

--- app/src/main/java/com/smilehunter/ablebody/domain/GetUserInfoUseCase.kt
+++ app/src/main/java/com/smilehunter/ablebody/domain/GetUserInfoUseCase.kt
@@ -4,22 +4,29 @@ import com.smilehunter.ablebody.data.dto.response.data.UserDataResponseData
 import com.smilehunter.ablebody.data.repository.UserRepository
 import com.smilehunter.ablebody.model.UserInfoData
 import javax.inject.Inject
+import com.smilehunter.ablebody.network.di.AbleBodyDispatcher.IO
+import com.smilehunter.ablebody.network.di.Dispatcher
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 
 class GetUserInfoUseCase @Inject constructor(
-    private val userRepository: UserRepository
+    @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
+    private val userRepository: UserRepository,
 ) {
-
-    suspend operator fun invoke(): UserInfoData =
+    suspend operator fun invoke(): UserInfoData = withContext(ioDispatcher) {
         userRepository.getMyUserData().data!!.toDomain()
+    }
 
 
-    suspend operator fun invoke(uid: String): UserInfoData =
+    suspend operator fun invoke(uid: String): UserInfoData = withContext(ioDispatcher) {
         userRepository.getUserData(uid).data!!.toDomain()
-
+    }
 }
 
-private fun UserDataResponseData.toDomain() =
-    UserInfoData(
+private fun UserDataResponseData.toDomain(): UserInfoData {
+    val isCreator = authorities.any { it.authorityName == "ROLE_CRT" }
+
+    return UserInfoData(
         createDate = createDate,
         modifiedDate = modifiedDate,
         gender = gender,
@@ -33,5 +40,6 @@ private fun UserDataResponseData.toDomain() =
         profileUrl = profileUrl,
         introduction = introduction,
         creatorPoint = creatorPoint,
-        authorities = authorities.map { UserInfoData.Authorities(it.authorityName) },
+        userType = if (isCreator) UserInfoData.UserType.CREATOR else UserInfoData.UserType.USER
     )
+}
