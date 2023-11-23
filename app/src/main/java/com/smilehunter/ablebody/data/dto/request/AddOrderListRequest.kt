93de81f3b9b662e7077699e16656f6dbfbcbdd01
MinJun <agvber12@gmail.com>
2023-11-23 11:57:32 +0900

    Modify: AddOrderListRequest 요청 body 변경

--- app/src/main/java/com/smilehunter/ablebody/data/dto/request/AddOrderListRequest.kt
+++ app/src/main/java/com/smilehunter/ablebody/data/dto/request/AddOrderListRequest.kt
@@ -1,18 +1,25 @@
 package com.smilehunter.ablebody.data.dto.request
 
 data class AddOrderListRequest(
-    val itemId: Int,
     val addressId: Int,
-    val couponBagsId: Int?,
-    val refundBankName: String,
-    val refundAccount: String,
-    val refundAccountHolder: String,
+    val orderName: String,
+    val paymentType: String,
     val paymentMethod: String,
-    val price: Int,
-    val itemDiscount: Int,
-    val couponDiscount: Int,
-    val pointDiscount: Int,
-    val deliveryPrice: Int,
-    val amountOfPayment: Int,
-    val itemOptionIdList: List<Long>?
-)
+    val easyPayType: String?,
+    val pointDiscount: Int?,
+    val deliveryPrice: Int?,
+    val amount: Int,
+    val orderListItemReqDtoList: List<OrderListItemReqDto>
+) {
+    data class OrderListItemReqDto(
+        val itemId: Int,
+        val couponBagsId: Int?,
+        val colorOption: String?,
+        val sizeOption: String?,
+        val itemPrice: Int,
+        val itemCount: Int,
+        val itemDiscount: Int,
+        val couponDiscount: Int,
+        val amount: Int
+    )
+}
