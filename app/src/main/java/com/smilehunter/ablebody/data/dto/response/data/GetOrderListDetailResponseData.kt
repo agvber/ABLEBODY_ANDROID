3abd3ed8ce62abdb0c81af4f206600f4adf21ba6
MinJun <agvber12@gmail.com>
2023-11-24 19:12:25 +0900

    Modify: getOrderListDetail api 변경 대응

--- app/src/main/java/com/smilehunter/ablebody/data/dto/response/data/GetOrderListDetailResponseData.kt
+++ app/src/main/java/com/smilehunter/ablebody/data/dto/response/data/GetOrderListDetailResponseData.kt
@@ -3,40 +3,50 @@ package com.smilehunter.ablebody.data.dto.response.data
 data class GetOrderListDetailResponseData(
     val orderListId: String,
     val orderedDate: String,
-    val itemName: String,
-    val itemImage: String,
     val orderStatus: OrderStatus,
-    val brand: String,
     val receiverName: String,
     val addressInfo: String,
     val addressDetail: String,
-    val itemOptionDetailList: List<ItemOptionDetailList>,
+    val orderListItemList: List<OrderItem>,
     val phoneNum: String,
-    val depositDeadline: String,
-    val price: Int,
+    val itemPrice: Int,
     val itemDiscount: Int,
     val couponDiscount: Int,
     val pointDiscount: Int,
     val deliveryPrice: Int,
-    val amountOfPayment: Int
+    val amount: Int
 ) {
-    data class ItemOptionDetailList(
-        val id: Int,
-        val orderList: String,
-        val itemOption: String,
-        val itemOptionDetail: String
+    data class OrderItem(
+        val itemName: String,
+        val itemImage: String,
+        val itemCount: Int,
+        val amount: Int,
+        val brand: String,
+        val colorOption: String?,
+        val sizeOption: String?
     )
 
     enum class OrderStatus {
-        DEPOSIT_WAITING,
-        DEPOSIT_COMPLETED,
-        ON_DELIVERY,
+        WAIT_FOR_PAYMENT,
+        CONFIRMATION_OF_DEPOSIT,
+        SHIPMENT_PROCESSING,
+        SHIPMENT_COMPLETED,
+        DELIVERY_STARTS,
         DELIVERY_COMPLETED,
+        CONFIRMATION_OF_PURCHASE,
+        PAYMENT_ERROR,
         ORDER_CANCELED,
-        REFUND_REQUEST,
-        REFUND_COMPLETED,
-        EXCHANGE_REQUEST,
-        ON_EXCHANGE_DELIVERY,
-        EXCHANGE_COMPLETED
+        EXCHANGE_ORDER_ACCEPTED,
+        REQUEST_FOR_EXCHANGE_RECOVERY,
+        EXCHANGE_RECOVERY_COMPLETED,
+        EXCHANGE_DELIVERY_COMPLETED,
+        EXCHANGE_PROCESSING,
+        EXCHANGE_COMPLETED,
+        EXCHANGE_CANCELED,
+        REQUEST_FOR_REFUND_RECOVERY,
+        REFUND_RECOVERY_COMPLETED,
+        REFUND_DELIVERY_COMPLETED,
+        REFUND_PROCESSING,
+        REFUND_COMPLETED
     }
 }
