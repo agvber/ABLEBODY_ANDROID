50be9359214fdd6bcdf37874a35bd296cdbbc1f5
MinJun <agvber12@gmail.com>
2023-12-01 15:49:55 +0900

    Feat: Bookmark 화면에 다양한 에러 핸들링을 추가하였습니다.

--- app/src/main/java/com/smilehunter/ablebody/presentation/home/bookmark/BookmarkGraph.kt
+++ app/src/main/java/com/smilehunter/ablebody/presentation/home/bookmark/BookmarkGraph.kt
@@ -2,11 +2,13 @@ package com.smilehunter.ablebody.presentation.home.bookmark
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.smilehunter.ablebody.model.ErrorHandlerCode
 import com.smilehunter.ablebody.presentation.home.bookmark.ui.BookmarkListRoute
 import com.smilehunter.ablebody.presentation.main.data.NavigationItems
 
 fun NavGraphBuilder.addBookmarkScreen(
     isBottomBarShow: (Boolean) -> Unit,
+    onErrorRequest: (ErrorHandlerCode) -> Unit,
     onSearchBarClick: () -> Unit,
     onAlertButtonClick: () -> Unit,
     onProductItemDetailRouteRequest: (Long) -> Unit,
@@ -14,6 +16,7 @@ fun NavGraphBuilder.addBookmarkScreen(
 ) {
     composable(route = NavigationItems.Bookmark.name) {
         BookmarkListRoute(
+            onErrorRequest = onErrorRequest,
             onSearchBarClick = onSearchBarClick,
             onAlertButtonClick = onAlertButtonClick,
             productItemClick = onProductItemDetailRouteRequest,
