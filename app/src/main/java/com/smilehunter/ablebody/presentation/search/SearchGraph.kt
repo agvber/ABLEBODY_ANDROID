82b6f9634fc6f55c1e901e1faba2dcdc234a09cf
MinJun <agvber12@gmail.com>
2023-12-01 21:42:11 +0900

    Feat: Search 화면에 다양한 에러 핸들링을 추가하였습니다.

--- app/src/main/java/com/smilehunter/ablebody/presentation/search/SearchGraph.kt
+++ app/src/main/java/com/smilehunter/ablebody/presentation/search/SearchGraph.kt
@@ -2,16 +2,19 @@ package com.smilehunter.ablebody.presentation.search
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.smilehunter.ablebody.model.ErrorHandlerCode
 import com.smilehunter.ablebody.presentation.search.ui.SearchRoute
 
 fun NavGraphBuilder.addSearchScreen(
     isBottomBarShow: (Boolean) -> Unit,
+    onErrorOccur: (ErrorHandlerCode) -> Unit,
     backRequest: () -> Unit,
     productItemClick: (Long) -> Unit,
     codyItemClick: (Long) -> Unit,
 ) {
     composable(route = "SearchRoute") {
         SearchRoute(
+            onErrorOccur = onErrorOccur,
             backRequest = backRequest,
             productItemClick = productItemClick,
             codyItemClick = codyItemClick,
