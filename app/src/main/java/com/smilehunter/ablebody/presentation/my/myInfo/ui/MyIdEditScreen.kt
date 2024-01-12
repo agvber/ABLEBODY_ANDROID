59b4ee3502afc0b2e41164ccdda62025f32aec33
nhkim <nhkim1529@gmail.com>
2024-01-13 03:58:02 +0900

    Feat: 휴대폰 번호 수정 페이지 에러 핸들링

--- app/src/main/java/com/smilehunter/ablebody/presentation/my/myInfo/ui/MyIdEditScreen.kt
+++ app/src/main/java/com/smilehunter/ablebody/presentation/my/myInfo/ui/MyIdEditScreen.kt
@@ -1,6 +1,7 @@
 package com.smilehunter.ablebody.presentation.my.myInfo.ui
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -20,12 +21,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -41,6 +44,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.smilehunter.ablebody.R
+import com.smilehunter.ablebody.model.ErrorHandlerCode
 import com.smilehunter.ablebody.presentation.my.myInfo.MyInfoEditViewModel
 import com.smilehunter.ablebody.presentation.onboarding.ui.InputPhoneNumberWithRuleLayout
 import com.smilehunter.ablebody.presentation.onboarding.OnboardingViewModel
@@ -54,6 +58,7 @@ import com.smilehunter.ablebody.ui.theme.AbleBlue
 import com.smilehunter.ablebody.ui.theme.AbleDark
 import com.smilehunter.ablebody.ui.utils.CustomLabelText
 import com.smilehunter.ablebody.ui.utils.CustomTextField
+import com.smilehunter.ablebody.ui.utils.SimpleErrorHandler
 import com.smilehunter.ablebody.ui.utils.TextFieldUnderNormalText
 import com.smilehunter.ablebody.ui.utils.TextFieldUnderText
 import kotlinx.coroutines.flow.launchIn
@@ -65,6 +70,7 @@ fun ChangePhoneNumberRoute(
 ) {
     val phoneNumberState by myInfoEditViewModel.phoneNumberState.collectAsStateWithLifecycle()
     val phoneNumberEnable by myInfoEditViewModel.isPhoneNumberCorrectState.collectAsStateWithLifecycle()
+    val errorData by myInfoEditViewModel.sendErrorLiveData.observeAsState()
 
     Log.d("phoneNumberState", phoneNumberState)
     ChangePhoneNumberScreen(
@@ -78,6 +84,11 @@ fun ChangePhoneNumberRoute(
             certificationBtnOnClick(phoneNumberState)
         }
     )
+
+    if (errorData != null) {
+        val context = LocalContext.current
+        Toast.makeText(context, "네트워크 에러가 발생했습니다\n다시 시도해주세요", Toast.LENGTH_LONG).show()
+    }
 }
 
 @Composable
