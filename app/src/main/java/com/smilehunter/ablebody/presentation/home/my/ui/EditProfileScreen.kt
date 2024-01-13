76c665472be84fc40fb8f999f6624aca71a396ab
MinJun <agvber12@gmail.com>
2024-01-13 19:36:42 +0900

    fix: 스크롤 안되는 문제 해결

--- app/src/main/java/com/smilehunter/ablebody/presentation/home/my/ui/EditProfileScreen.kt
+++ app/src/main/java/com/smilehunter/ablebody/presentation/home/my/ui/EditProfileScreen.kt
@@ -20,11 +20,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material3.BottomSheetDefaults
@@ -300,8 +302,10 @@ fun EditProfileScreen(
             }
         }
     ) { paddingValue ->
+        val scrollState = rememberScrollState()
         Column(
             modifier = Modifier
+                .verticalScroll(scrollState)
                 .padding(paddingValue)
         ) {
             Box(
